25cf84819fccbfb5513d1d99b86878365c69db1d
Tim Parkyn <timparkyn@gmail.com>
2017-11-14 13:13:09 -0800

    First commit.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,20 +2,22 @@ var App = () => (
   <div>
     <nav className="navbar">
       <div className="col-md-6 offset-md-3">
-        <div><h5><em>search</em> view goes here</h5></div>
+        <div><h5><em>search</em><Search /></h5></div>
       </div>
     </nav>
     <div className="row">
       <div className="col-md-7">
-        <div><h5><em>videoPlayer</em> view goes here</h5></div>
+        <VideoPlayer />
       </div>
       <div className="col-md-5">
-        <div><h5><em>videoList</em> view goes here</h5></div>
+        <VideoList videos={window.exampleVideoData}/>
       </div>
     </div>
   </div>
 );
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
-// `var` declarations will only exist globally where explicitly defined
+// `var` declarations will only exist globally where explicitly defined ohyeah
 window.App = App;
+
+//
